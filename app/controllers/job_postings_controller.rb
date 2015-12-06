663dd2a7a29122d4175c3c17e167039f4e2598d1
Chrissie Deist <chrissie.deist@gmail.com>
2015-12-06 10:52:56 -0600

    add index and show for job posting

--- app/controllers/job_postings_controller.rb
+++ app/controllers/job_postings_controller.rb
@@ -1,3 +1,9 @@
 class JobPostingsController < ApplicationController
+  def index
+    @job_postings = JobPosting.all
+  end
 
+  def show
+    @job_posting = JobPosting.find(params[:id])
+  end
 end
--- spec/controllers/job_postings_controller_spec.rb
+++ spec/controllers/job_postings_controller_spec.rb
@@ -1,8 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe JobPostingsController, :type => :controller do
+  describe "#index" do
+    it "displays all job postings" do
+      10.times { FactoryGirl.create(:job_posting) }
+      get :index
 
-  it "creates a job posting associated with the current user" do
+      expect(response.status).to be 200
+      postings = assigns(:job_postings)
+      expect(postings.count).to eq(10)
+    end
   end
 
+  describe "#show" do
+    it "displays a job posting" do
+      FactoryGirl.create(:job_posting, :title => "Developer")
+
+      get :show, :id => 1
+
+      expect(response.status).to be 200
+      posting = assigns(:job_posting)
+      expect(posting.title).to eq("Developer")
+      expect(response).to match(/Developer/)
+    end
+  end
 end
