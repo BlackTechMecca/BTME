0458b0f396b75a620535727e64d0bf51175c57c1
Andrew Carr <aacarr5@gmail.com>
2015-12-07 17:36:15 -0600

    Create event model

---
+++ app/models/event.rb
@@ -0,0 +1,20 @@
+class Event < ActiveRecord::Base
+	include Postable
+
+	belongs_to :poster, class_name: "User", foreign_key: "user_id"
+
+	has_one :activity, 
+			:as => :postable,
+			:foreign_key => :postable_id
+
+	validates :name, :description, :date, :presence => true
+
+	after_save :_create_activity
+
+	def poster_name
+		user.full_name
+	end
+
+
+
+end
