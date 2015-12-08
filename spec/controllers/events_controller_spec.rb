a465ed1de7147cafd6fc250b3a8ec0b0523d6423
Andrew Carr <aacarr5@gmail.com>
2015-12-08 15:50:09 -0600

    Create inital index test

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe EventsContoller, :type => :controller do 
+RSpec.describe EventsController, :type => :controller do 
 	describe '#index' do 
 		it 'displays all events' do 
 			10.times {FactoryGirl.create(:event)}
