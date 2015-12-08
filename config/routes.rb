a83d5b03147aa510085caf17e9b6d119229dc013
Andrew Carr <aacarr5@gmail.com>
2015-12-08 15:27:00 -0600

    Add events routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   get 'activity', to: 'activities#index'
 
+  resources :events, only: [:show,:index]
+
   root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
