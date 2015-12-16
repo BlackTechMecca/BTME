b772968e7c97599db83fea4321e3f24a75191a31
Andrew Carr <aacarr5@gmail.com>
2015-12-15 20:34:43 -0600

    Add event specs

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe Event, :type => :model do
 	let(:event) {FactoryGirl.create(:event, :name=>"Chi-hacknight",:description => "A place where stuff happens",:user=>user)}
 
 	describe '#initialize' do 
-
 		it '#initialize should set that proper characteristics' do 
 			expect(event.name).to eq("Chi-hacknight")
 			expect(event.description).to eq("A place where stuff happens")
@@ -15,8 +14,23 @@ RSpec.describe Event, :type => :model do
 		it 'create an associated activity' do 
 			expect(event.activity).to be_a Activity
 		end
+	end
+
+
+	describe 'validations' do 
+		it 'validates that it has a user' do 
+			invalid_event = Event.create(name:"A presentation",description:"some some")
+			expect(invalid_event).to_not be_valid
+		end
+
+		it 'validates that it has a name' do 
+			invalid_event = Event.create(description:"something more",user:FactoryGirl.create(:user))
+			expect(invalid_event).to_not be_valid
+		end
 
 	end
+
+
 end
 
 	
